eec14be243d25a9cbd33b1692301056da304e7b1
Robin Pahlman <robin.pahlman@gmail.com>
2017-09-26 23:17:19 +0000

    add #list_articles to cli.rb, hardcoded articles for testing purposes

--- lib/tech_news/cli.rb
+++ lib/tech_news/cli.rb
@@ -4,5 +4,15 @@ class TechNews::CLI
 
   def call
     puts "The Latest Tech News:"
+    list_articles
+  end
+
+  def list_articles
+    # here doc 
+    puts <<-DOC
+      1. Chief executive ousted at Equifax following data breach
+      2. Why, Twitter, Why?
+      3. Microsoft tries to stem its self-made collaboration-tool confusion
+    DOC
   end
 end
