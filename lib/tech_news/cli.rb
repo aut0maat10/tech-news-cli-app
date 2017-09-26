adfd967b07ee13c9f1a8a30c70079c93fc03ed5b
Robin Pahlman <robin.pahlman@gmail.com>
2017-09-26 23:38:57 +0000

    add methods #menu and #goodbye to cli.rb to get interface up and running

--- lib/tech_news/cli.rb
+++ lib/tech_news/cli.rb
@@ -3,16 +3,43 @@
 class TechNews::CLI
 
   def call
-    puts "The Latest Tech News:"
     list_articles
+    menu
+    goodbye
   end
 
   def list_articles
-    # here doc 
+    puts "The Latest Tech News:"
+    # here doc
     puts <<-DOC
       1. Chief executive ousted at Equifax following data breach
       2. Why, Twitter, Why?
       3. Microsoft tries to stem its self-made collaboration-tool confusion
     DOC
+  end
+
+  def menu
+
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the article you'd like to read, type list to see the list of articles again, or type exit:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Here's article 1..."
+      when "2"
+        puts "Here's article 2..."
+      when "3"
+        puts "Here's article 3..."
+      when "list"
+        list_articles
+      else
+        puts "Not sure what you want, type list or exit."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Check in again for more tech articles!"
   end
 end
