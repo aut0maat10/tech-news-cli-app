3e8dbb933d9fffac26e8cf9b596d81f765bb9034
Robin Pahlman <robin.pahlman@gmail.com>
2017-09-27 15:15:36 +0000

    add abstraction and functionality to cli

--- lib/tech_news/cli.rb
+++ lib/tech_news/cli.rb
@@ -1,5 +1,4 @@
 # CLI Controller
-
 class TechNews::CLI
 
   def call
@@ -11,22 +10,21 @@ class TechNews::CLI
   def list_articles
     puts "The Latest Tech News:"
     @articles = TechNews::Article.all
+    @articles.each.with_index(1) do |article, i|
+      puts "#{i}. #{article.title}" # add source/publication???
+    end
   end
 
   def menu
-
     input = nil
     while input != "exit"
       puts "Enter the number of the article you'd like to read, type list to see the list of articles again, or type exit:"
       input = gets.strip.downcase
-      case input
-      when "1"
-        puts "Here's article 1..."
-      when "2"
-        puts "Here's article 2..."
-      when "3"
-        puts "Here's article 3..."
-      when "list"
+
+      if input.to_i > 0
+        the_article = @articles[input.to_i - 1]
+        puts "#{the_article.title}"
+      elsif input == "list"
         list_articles
       else
         puts "Not sure what you want, type list or exit."
